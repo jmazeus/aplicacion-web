816cb359a8171135b82b28942207d523bc38c228
jmazeus <mazeus69@hotmail.com>
2023-01-08 11:05:53 -0600

    Initial push

---
+++ src/main/java/com/tresct/aplicacionweb/PersonaController.java
@@ -0,0 +1,16 @@
+package com.tresct.aplicacionweb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v2/personas")
+public class PersonaController {
+    @GetMapping
+    public List<Persona> getPersonas(){
+        return List.of(new Persona("César", 10));
+    }
+}
---
+++ src/test/java/com/tresct/aplicacionweb/AplicacionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tresct.aplicacionweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
